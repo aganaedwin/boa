39ffa70e4a7f96b38ff8aafe09af2be0e8cd4c16
Agana Edwin <aganaedwin1@gmail.com>
2022-02-20 21:47:22 +0000

    zelle 2nd modification

--- boa.sh
+++ boa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-       echo "Please enter your name"
-       read name
+       echo "Please enter your username"
+       read username
        echo "Please enter your password"
        read -s password
        echo "Welcome to the boa zelle application"
